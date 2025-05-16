53d3fc9f0abc7eec958001e2da32e54f66df5910
Hendrik Hassa <65833107+HennieLP@users.noreply.github.com>
2025-05-16 23:35:00 +0200

    select menu

---
+++ scripts/menuController.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+@export var Player = 0
+var controlls = [
+ 	["player0_right", "player0_up", "player0_down","player0_upgrade"],
+	["player1_left", "player1_up", "player1_down","player1_upgrade"]
+]
+
+@onready var selected = $".".get_children()[0]
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed(controlls[Player][1]):
+		selected.get_child(0).visible = false
+		selected = selected.prev
+		selected.get_child(0).visible = true
+	if event.is_action_pressed(controlls[Player][2]):
+		selected.get_child(0).visible = false
+		selected = selected.next
+		selected.get_child(0).visible = true
+	if event.is_action_pressed(controlls[Player][3]):
+		if visible:
+			visible = false
+		else:
+			visible = true
+	if event.is_action_pressed(controlls[Player][0]):
+		if visible == true:
+			print("Call effetct")
+			visible = false
+		
---
+++ scripts/meueItem.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+@export var prev :Node2D
+@export var next : Node2D
