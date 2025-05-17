1a368d7765eac4c0704078d1360c82757cf4b7ff
Hendrik Hassa <65833107+HennieLP@users.noreply.github.com>
2025-05-17 18:36:48 +0200

    henneHatkeineAhnungWasErGeenderHat

---
+++ scripts/SelectMenueLable.gd
@@ -0,0 +1,6 @@
+extends Label
+
+@export var type : Gameconstants.Attack
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	text = text + " ("+ str(Gameconstants.AttackCosts[type]) +")"
