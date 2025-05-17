d3c9b4185b4e8649ffbf1284c3bcd4166054b23a
Felix <felix@weglehner.info>
2025-05-17 23:48:24 +0200

    Warning

---
+++ scripts/death_warning.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@export var Player: int = 0
+
+func _ready() -> void:
+	Gamestate.players[Player].utilization_updated.connect(update)
+	if Player == 1:
+		$AnimatedSprite2D.flip_h = true
+	$AnimatedSprite2D.play()
+	
+func update(utilization: float) -> void:
+	visible = utilization >= 1 
